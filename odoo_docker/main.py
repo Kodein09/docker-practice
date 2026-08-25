dacd788ca9d4a3903687c6bccae8ab5ba97e9f73
Roy Ashba <rashba@psystems.biz>
2026-08-25 18:51:34 +0500

    feat: Initial odoo commit

---
+++ odoo_docker/main.py
@@ -0,0 +1,9 @@
+def climb_stairs(n: int) -> int:
+    one, two = 1, 1
+    for i in range(n - 1):
+        temp = one
+        one = one + two
+        two = temp
+    return one
+
+print(climb_stairs(5))
